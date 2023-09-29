4d3d3e1f3f21009ce1a4e13995d6ab61ad7193e2
Priya <145586822+Priyasa2003@users.noreply.github.com>
2023-09-29 13:39:36 +0530

    Add files via upload

---
+++ primarycalculation.java
@@ -0,0 +1,14 @@
+//import java.util.Scanner;
+class primarycalculation
+{
+    public static void main(String[] args)
+    {
+    //Scanner sc = new Scanner(System.in);
+      int a=5,b=10; 
+      System.out.println((a+b));  
+      System.out.println((a-b));
+      System.out.println((a*b)); 
+      System.out.println((a/b));
+
+    }   
+}
